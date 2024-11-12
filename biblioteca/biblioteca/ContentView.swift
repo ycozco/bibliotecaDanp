dfd957977ca3c3795975c7cd1d2f58550be86d89
yosetcozco <ycozco@unsa.edu.pe>
2024-11-12 14:12:34 -0500

    initial files

---
+++ biblioteca/biblioteca/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  biblioteca
+//
+//  Created by yoset on 22/08/1403 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ biblioteca/biblioteca/bibliotecaApp.swift
@@ -0,0 +1,17 @@
+//
+//  bibliotecaApp.swift
+//  biblioteca
+//
+//  Created by yoset on 22/08/1403 AP.
+//
+
+import SwiftUI
+
+@main
+struct bibliotecaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
